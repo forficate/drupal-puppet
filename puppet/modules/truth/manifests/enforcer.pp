7f04908041c9b608f96f20bdc92659e17b4c289c
Adam Evans <adam@agileand.me>
2013-01-17 23:37:20 +0000

    Update puppet/modules/truth/manifests/enforcer.pp
    Code formatting

--- puppet/modules/truth/manifests/enforcer.pp
+++ puppet/modules/truth/manifests/enforcer.pp
@@ -1,14 +1,14 @@
 class truth::enforcer {
 	
-	if has_role("drupal_stack") {
-	  notice("I am a fully fledged drupal stack baby")
-	  include nginx::php
+    if has_role("drupal_stack") {
+        notice("I am a fully fledged drupal stack baby")
+        include nginx::php
 
-	  class { 'mysql::server':
-      	config_hash => { 'root_password' => 'changeme' }
-      }
+        class { 'mysql::server':
+            config_hash => { 'root_password' => 'changeme' }
+        }
       
-	}
+    }
 
 
 }
