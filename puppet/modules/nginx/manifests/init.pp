ccff971f67c7f5b79d7956f5a6bb6ef20159a829
Adam Evans <adam@agileand.me>
2013-01-17 22:54:31 +0000

    Updated default nginx to hide servertokens. It's not a security risk showing the tokens but it makes that little bit harder for a hacker as they can't see specific version of nginx we are running against known exploits. They now need to try and figure out the version...more work.

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -8,4 +8,14 @@ class nginx {
 		ensure => running,
 		subscribe => Package['nginx'],
 	}
+
+ 	file {"/etc/nginx/nginx.conf":
+	    source => "puppet:///modules/nginx/nginx.conf",
+	    owner => 'root',
+	    group => 'root',
+	    mode => '644',
+	    require => Package['nginx'],
+	    notify => Service['nginx'],
+  	}
+
 }
