3074fa82754e95eb3e17eaa2de63f97813b28d54
Adam Evans <adam@agileand.me>
2013-01-17 23:06:33 +0000

    Added mysql with default root password as changeme. Yes it's bad, need to work on externalising passwords so externally configurable.

--- puppet/modules/truth/manifests/enforcer.pp
+++ puppet/modules/truth/manifests/enforcer.pp
@@ -3,6 +3,11 @@ class truth::enforcer {
 	if has_role("drupal_stack") {
 	  notice("I am a fully fledged drupal stack baby")
 	  include nginx::php
+
+	  class { 'mysql::server':
+      	config_hash => { 'root_password' => 'changeme' }
+      }
+      
 	}
 
 
