2eb9dae63ea82a83078a197e54866521c2b1fc87
Adam Evans <adam@agileand.me>
2013-01-19 01:14:24 +0000

    Added git instalation

---
+++ puppet/modules/drupal_stack/manifests/drush.pp
@@ -0,0 +1,24 @@
+class drush {
+	
+	package {"php-pear":
+		ensure => present,
+	}
+
+	exec {"add_drush_pear_channel":
+	    command => "pear channel-discover pear.drush.org",
+	    unless => "pear list-channels | grep pear.drush.org",
+	    require => Package['php-pear']
+	}
+
+	exec {"install_drush":
+		command => "pear install drush/drush",
+		require => Exec["add_drush_pear_channel"],
+		unless => "pear list -c pear.drush.org | grep drush",
+	}
+
+	exec {"install_console_table":
+		command => "pear install Console_Table",
+		require => Package['php-pear'],
+		unless => "pear list | grep Console_Table",
+	}
+}
--- puppet/modules/drupal_stack/manifests/init.pp
+++ puppet/modules/drupal_stack/manifests/init.pp
@@ -1,6 +1,7 @@
 class drupal_stack {
     include secured
     include nginx::php
+    include drush
 
     class { 'mysql::server':
         config_hash => { 'root_password' => 'changeme' }
