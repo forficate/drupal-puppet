cf2917889facd9c94c37e478b01f1647df6662da
Adam Evans <adam@agileand.me>
2013-01-19 00:04:07 +0000

    Committed file missed in last commit

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,3 +1,13 @@
+Exec {
+    path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+}
+
+Exec["apt-update"] -> Package <| |>
+
+exec { "apt-update":
+    command => "/usr/bin/apt-get update"
+}
+
 node default {
   include truth::enforcer
 }
